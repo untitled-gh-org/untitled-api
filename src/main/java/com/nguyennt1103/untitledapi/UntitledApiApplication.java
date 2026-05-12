acc5cbd86b39a071cdab06ba842af416a50acd7c
nguyennt1103 <nguyen.nt1103@gmail.com>
2026-05-12 21:57:27 +0700

    Initial commit

---
+++ src/main/java/com/nguyennt1103/untitledapi/UntitledApiApplication.java
@@ -0,0 +1,13 @@
+package com.nguyennt1103.untitledapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UntitledApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UntitledApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nguyennt1103/untitledapi/UntitledApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nguyennt1103.untitledapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UntitledApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
